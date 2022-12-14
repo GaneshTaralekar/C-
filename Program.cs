5b4c9c04e5c116345e9c79e7dc8b0a3391d7d05a
Ganesh Taralekar <91971905+GaneshTaralekar@users.noreply.github.com>
2022-12-14 10:27:33 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number1, number2;
+int add, sub, mul;
+float div;
+Console.WriteLine("Enter  Frist Number :");
+number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Secound Number :");
+number2 = Convert.ToInt32(Console.ReadLine());
+add = number1 + number2;
+sub = number1 - number2;
+mul = number1 * number2;
+div = number1 / number2;
+Console.WriteLine("The addition is " + add);
+Console.WriteLine("The addition is " + sub);
+Console.WriteLine("The addition is " + mul);
+Console.WriteLine("The addition is " + div);
+Console.ReadLine();
+
+
+
+
+
+
